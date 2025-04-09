f9d09c15f13d7555aa6b13e1007f32ccf51dc3dc
Guruk257 <guruprasad.kudte@intiant.com>
2025-04-09 08:14:09 +0530

    fixed deploy errors:

--- auth.config.ts
+++ auth.config.ts
@@ -8,23 +8,6 @@ import Google from "next-auth/providers/google";
 
 export default {
   providers: [
-    Github({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope:
-            "openid email profile https://www.googleapis.com/auth/spreadsheets",
-          response_type: "code",
-          code_challenge_method: "S256", // Explicitly set PKCE method
-          prompt: "consent select_account",
-        },
-      },
-    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
